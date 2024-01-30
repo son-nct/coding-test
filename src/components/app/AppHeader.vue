7a51c69bbbf405672182bdcacb5ddafe96453019
sonnguyen <sonstarnguyen@gmail.com>
2024-01-31 01:16:42 +0700

    fix: update logo header

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -5,7 +5,7 @@ import { RouterLink } from 'vue-router'
 <template>
   <div classname="container mx-auto px-4 md:px-6 lg:px-8">
     <header class="flex items-center w-full h-20 px-4 border-b shrink-0 md:px-6">
-      <RouterLink class="hidden mr-6 lg:flex" to="/products">THAI SON</RouterLink>
+      <RouterLink class="flex mr-6" to="/products">THAI SON</RouterLink>
     </header>
   </div>
 </template>
